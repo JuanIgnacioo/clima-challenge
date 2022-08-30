8d054c290f3e2a2f553d7cd531784928835a82a5
Juan Ignacio <jcurtoni@pickit.net>
2022-08-30 16:01:57 -0300

    Se corrige error en skeleton

--- frontend/src/Components/ContentLoader/CardContentLoader.tsx
+++ frontend/src/Components/ContentLoader/CardContentLoader.tsx
@@ -4,8 +4,8 @@ import ContentLoader from "react-content-loader";
 export const CardContentLoader: React.FC = () => {
   return (
     <ContentLoader
-      viewBox="0 0 1360 900"
-      height={900}
+      viewBox="0 0 1360 300"
+      height={300}
       width={1360}
       data-testid="cardcontentloader-id"
     >
@@ -27,24 +27,6 @@ export const CardContentLoader: React.FC = () => {
       <rect x="1130" y="20" rx="8" ry="8" width="200" height="200" />
       <rect x="1130" y="250" rx="0" ry="0" width="200" height="18" />
       <rect x="1130" y="275" rx="0" ry="0" width="120" height="20" />
-      <rect x="30" y="340" rx="8" ry="8" width="200" height="200" />
-      <rect x="30" y="570" rx="0" ry="0" width="200" height="18" />
-      <rect x="30" y="595" rx="0" ry="0" width="120" height="20" />
-      <rect x="250" y="340" rx="8" ry="8" width="200" height="200" />
-      <rect x="250" y="570" rx="0" ry="0" width="200" height="18" />
-      <rect x="250" y="595" rx="0" ry="0" width="120" height="20" />
-      <rect x="470" y="340" rx="8" ry="8" width="200" height="200" />
-      <rect x="470" y="570" rx="0" ry="0" width="200" height="18" />
-      <rect x="470" y="595" rx="0" ry="0" width="120" height="20" />
-      <rect x="690" y="340" rx="8" ry="8" width="200" height="200" />
-      <rect x="690" y="570" rx="0" ry="0" width="200" height="18" />
-      <rect x="690" y="595" rx="0" ry="0" width="120" height="20" />
-      <rect x="910" y="340" rx="8" ry="8" width="200" height="200" />
-      <rect x="910" y="570" rx="0" ry="0" width="200" height="18" />
-      <rect x="910" y="595" rx="0" ry="0" width="120" height="20" />
-      <rect x="1130" y="340" rx="8" ry="8" width="200" height="200" />
-      <rect x="1130" y="570" rx="0" ry="0" width="200" height="18" />
-      <rect x="1130" y="595" rx="0" ry="0" width="120" height="20" />
     </ContentLoader>
   );
 };
